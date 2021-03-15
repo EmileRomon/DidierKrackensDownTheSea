4c19570982006d0fb4112173fd34f1d60e5f6df8
Emile Romon <emile.romon@gmail.com>
2021-03-15 15:23:26 +0100

    Event basic UI

--- Assets/Scripts/Events/Event.cs
+++ Assets/Scripts/Events/Event.cs
@@ -10,10 +10,12 @@ public class Event : ScriptableObject
     [SerializeField] private string _eventDescription;
     [SerializeField] private List<EventOption> _eventOptions;
     [SerializeField] private List<EventOutcome> _eventOutcomes;
+    [Range(0, 1)] [SerializeField] private float _eventProbability;
+
 
     public string EventName => _eventName;
     public string EventDescription => _eventDescription;
     public List<EventOption> EventOptions => _eventOptions;
     public List<EventOutcome> EventOutcomes => _eventOutcomes;
-
+    public float EventProbability => _eventProbability;
 }
--- Assets/Scripts/Events/EventOption.cs
+++ Assets/Scripts/Events/EventOption.cs
@@ -1,19 +1,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
-public class ProbableEvent
-{
-    [SerializeField] private Event _event;
-    [Range(0, 1)] [SerializeField] private float _probability;
-}
-
 [CreateAssetMenu(fileName = "EventOption", menuName = "Environment/Events/EventOption")]
 public class EventOption : ScriptableObject
 {
     [SerializeField] private string _optionDescription;
-    [SerializeField] private List<ProbableEvent> _optionSubEvents;
+    [SerializeField] private List<Event> _optionSubEvents;
 
     public string OptionDescription => _optionDescription;
-    public List<ProbableEvent> OptionSubEvents => _optionSubEvents;
+    public List<Event> OptionSubEvents => _optionSubEvents;
 }
